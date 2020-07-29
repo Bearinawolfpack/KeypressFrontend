732b1bad2cafc2290728227f6e70c7bd425e92e4
dolly-d <dolly.desir@gmail.com>
2020-07-28 20:04:37 -0400

    still killing it

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     const url = 'http://localhost:3000/playlists'
     
-    const mySound = document.getElementById("sound");
-    const correctButton = document.getElementById("correct");
-    correctButton.addEventListener("click", function(){ mySound.play(); }); 
+ 
 
     fetch(url)
     .then(resp => resp.json())
     .then(playlists => playlists)
 
-    const playSound = document.getElementById('sound')
     
 
 
 	document.addEventListener("keydown", (event) => {
-        if(event.code === 'KeyT'){
+        const sound = event.code
+        const playSound = document.getElementById(sound)
+        console.log(playSound)
             playSound.play()
-        }
-	});
+        
+    });
+    
+
+
+
+
+
+
+
+
+
 });
+
