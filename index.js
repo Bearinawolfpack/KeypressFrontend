f43f88e2dbda1c7d4d084b1bede9f3c736e2f802
dolly-d <dolly.desir@gmail.com>
2020-07-29 16:35:38 -0400

    killed it almost

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     const url = 'http://localhost:3000/playlists'
+    const userForm = document.querySelector('form')
+    console.log(userForm)
     
  
 
@@ -7,17 +9,86 @@ document.addEventListener("DOMContentLoaded", (event) => {
     .then(resp => resp.json())
     .then(playlists => playlists)
 
+    function postUser(url, newUserObj){
+        fetch(url, {
+            method: "POST",
+            headers:
+            {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(newUserObj)
+        })
+        .then(resp => resp.json())
+        .then(addedUser => renderNewUser(addedUser))
+    }
+    
+    userForm.addEventListener('submit', (e)=>{
+        console.log(e)
+    })
     
 
+    
+    // function postMonster(url, newMonsterObject){
+    //     fetch(url, {
+    //         method: "POST",
+    //         headers: 
+    //         {
+    //         "Content-Type": "application/json",
+    //         "Accept": "application/json"
+    //         },
+    //         body: JSON.stringify(newMonsterObject)
+    //     })
+    //     .then(resp => resp.json())
+    //     .then(monsterThatWasAdded => renderMonster(monsterThatWasAdded))
+    // }
 
-	document.addEventListener("keydown", (event) => {
-        const sound = event.code
-        const playSound = document.getElementById(sound)
-        console.log(playSound)
-            playSound.play()
+    // monsterCollection.addEventListener('submit', function(e){
+    //     e.preventDefault()
+
+    //     const newMonster = {
+    //         name: e.target.querySelector('#name').value,
+    //         age: e.target.querySelector('#age').value,
+    //         description: e.target.querySelector('#description').value
+    //     }
+
+    //     postMonster('http://localhost:3000/monsters', newMonster)
+    //     monsterCollection.reset()
+    // })
+
+
+
+
+
+
+
+
+
+    
+	// document.addEventListener("keydown", (event) => {
+    //     const sound = event.code
+    //     // console.log(event.code)
+    //     const playSound = document.getElementById(sound)
+    //     const fancySpan = document.getElementById('fancy-span')
+    //     fancySpan.innerText = event.key.toUpperCase()
+    //      playSound.play()
         
-    });
+    // });
     
+    // document.addEventListener('click', (e) => {
+    //     // if(){}
+    //     // console.dir(e.target.parentElement.attributes[1])
+    //     const keyboardLi = document.querySelector('li')
+    //     // console.log(keyboardLi)
+    //     // console.log(e.target.parentElement)
+    //     const fancySpan = document.getElementById('fancy-span')
+    //     const eventTarget = e.target.innerText
+    //     const playSound = document.getElementById(`Key${eventTarget}`)
+    //     playSound.play()
+    //      fancySpan.innerText = eventTarget
+
+    //     console.log(e.target.innerText)
+    //  })
 
 
 
